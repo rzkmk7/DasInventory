8497915b6a69d64d4a1ad7179b3989330c60d613
RizkiMaulanaKusuma <52888303+rizki7771@users.noreply.github.com>
2022-08-21 07:44:15 +0700

    update

---
+++ app/src/main/java/com/XyzStudio/dasinventory/AdapterInventory.java
@@ -0,0 +1,41 @@
+package com.XyzStudio.dasinventory;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class AdapterInventory<context> extends ArrayAdapter<InventoryData> {
+
+    Context context;
+    public List<InventoryData> arrayListInventory;
+
+    public AdapterInventory(@NonNull Context context, List<InventoryData> arrayListInventory) {
+        super(context, R.layout.laporan_item, arrayListInventory);
+
+        this.context = context;
+        this.arrayListInventory = arrayListInventory;
+    }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.laporan_item, null, true);
+
+        TextView tv_namaBarang = view.findViewById(R.id.tv_namaBarang);
+        TextView tv_type = view.findViewById(R.id.tv_type);
+        TextView tv_jmlStok = view.findViewById(R.id.tv_jmlStok);
+
+        tv_namaBarang.setText(arrayListInventory.get(position).getNamaBarang());
+        tv_type.setText(arrayListInventory.get(position).getType());
+        tv_jmlStok.setText(arrayListInventory.get(position).getJmlStok());
+
+
+        return view;
+    }
+}
--- app/src/main/java/com/XyzStudio/dasinventory/Inventory.java
+++ app/src/main/java/com/XyzStudio/dasinventory/Inventory.java
@@ -1,33 +1,96 @@
 package com.XyzStudio.dasinventory;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Inventory extends AppCompatActivity {
 
     FloatingActionButton fab_add_inventory;
+
+    ListView listView;
+    AdapterInventory adapter;
+    public static ArrayList<InventoryData> inventoryArrayList = new ArrayList<>();
+    Context context;
+
+    public FirebaseDatabase database;
+    public DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
 
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Inventory");
+
+        context = this;
         fab_add_inventory = findViewById(R.id.fab_add_inventory);
+
+        adapter = new AdapterInventory(this, inventoryArrayList);
+        adapter.arrayListInventory.clear();
+
+        listView = findViewById(R.id.ListInventory);
+        listView.setAdapter(adapter);
+
 
         fab_add_inventory.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                /* startActivity(new Intent(Inventory.this,FormInventory.class));*/
-                FormInventory formInventory = new FormInventory(/*"l"*/);
+                FormInventory formInventory = new FormInventory();
                 formInventory.show(getSupportFragmentManager(), "activity_form_inventory");
             }
         });
+        getData();
     }
+    public void getData(){
+        inventoryArrayList.clear();
+        ref.addChildEventListener(new ChildEventListener(){
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                InventoryData inventory = snapshot.getValue(InventoryData.class);
+                inventoryArrayList.add(inventory);
+                /*adapter.notifyDataSetChanged();*/ //teeuing
 
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
 }
