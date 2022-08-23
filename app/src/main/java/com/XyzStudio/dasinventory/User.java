278b1e21dca572da4ed8a2e0a7a28df98dadff53
RizkiMaulanaKusuma <52888303+rizki7771@users.noreply.github.com>
2022-08-23 19:36:44 +0700

    ic logout

--- app/src/main/java/com/XyzStudio/dasinventory/User.java
+++ app/src/main/java/com/XyzStudio/dasinventory/User.java
@@ -3,14 +3,21 @@ package com.XyzStudio.dasinventory;
 public class User {
 
     public String  nama;
+    public String  jabatan;
+
+
+
 
 
     public User(){
 
     }
 
-    public User(String nama){
+    public User(String nama, String jabatan ){
+
         this.nama=nama;
+        this.jabatan=jabatan;
+
     }
 
 }
