9880e19b84342b88b28760cd2cd531d68bffec95
Anshor15 <79810135+YuukiRYT@users.noreply.github.com>
2022-08-27 21:24:31 +0700

    Add export to excel

--- app/src/main/java/com/XyzStudio/dasinventory/Inventory.java
+++ app/src/main/java/com/XyzStudio/dasinventory/Inventory.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,10 +32,22 @@ import com.google.firebase.database.ValueEventListener;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Inventory extends AppCompatActivity {
 
@@ -153,11 +166,13 @@ public class Inventory extends AppCompatActivity {
         refInv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                searchByName("Jaa");
+//                searchByName("Jaa");
 //                finish();
 //                overridePendingTransition(1, 1);
 //                startActivity(getIntent());
 //                overridePendingTransition(1, 1);
+
+                exportDataIntoWorkbook(context, "test.xlsx", inventoryArrayList);
             }
         });
         getData();
@@ -296,7 +311,7 @@ public class Inventory extends AppCompatActivity {
 //                            Log.d("testa", inventory.getNamaBarang());
 //                            Log.d("testa", name);
 //                            Log.d("testa", String.valueOf(inventory.getNamaBarang().equals(name)));
-                            if(inventory.getNamaBarang().equals(name))
+                            if(inventory.getNamaBarang().equals(name) || inventory.getDate().equals(name))
                             {
                                 inventory.setKey(snapshot.getKey());
                                 inventoryArrayList.add(inventory);
@@ -313,4 +328,174 @@ public class Inventory extends AppCompatActivity {
         });
     }
 
+    // region Export Excel
+
+    private static Cell cell;
+    private static Sheet sheet;
+
+    private static String EXCEL_SHEET_NAME = "Sheet1";
+    private static Workbook workbook;
+    private static CellStyle headerCellStyle;
+
+    public static boolean exportDataIntoWorkbook(Context context, String fileName,
+                                                 List<InventoryData> dataList) {
+        boolean isWorkbookWrittenIntoStorage;
+
+        // Check if available and not read only
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            Log.e("testa", "Storage not available or read only");
+            return false;
+        }
+
+        // Creating a New HSSF Workbook (.xls format)
+        workbook = new HSSFWorkbook();
+
+        setHeaderCellStyle();
+
+        // Creating a New Sheet and Setting width for each column
+        sheet = workbook.createSheet("Sheet1");
+        sheet.setColumnWidth(0, (15 * 400));
+        sheet.setColumnWidth(1, (15 * 400));
+        sheet.setColumnWidth(2, (15 * 400));
+        sheet.setColumnWidth(3, (15 * 400));
+
+        setHeaderRow();
+        fillDataIntoExcel(dataList);
+        isWorkbookWrittenIntoStorage = storeExcelInStorage(context, fileName);
+
+        return isWorkbookWrittenIntoStorage;
+    }
+
+    /**
+     * Checks if Storage is READ-ONLY
+     *
+     * @return boolean
+     */
+    private static boolean isExternalStorageReadOnly() {
+        String externalStorageState = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED_READ_ONLY.equals(externalStorageState);
+    }
+
+    /**
+     * Checks if Storage is Available
+     *
+     * @return boolean
+     */
+    private static boolean isExternalStorageAvailable() {
+        String externalStorageState = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(externalStorageState);
+    }
+
+    /**
+     * Setup header cell style
+     */
+    private static void setHeaderCellStyle() {
+        headerCellStyle = workbook.createCellStyle();
+        headerCellStyle.setFillForegroundColor(HSSFColor.AQUA.index);
+        headerCellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        headerCellStyle.setAlignment(CellStyle.ALIGN_CENTER);
+    }
+
+    /**
+     * Setup Header Row
+     */
+    private static void setHeaderRow() {
+        Row headerRow = sheet.createRow(0);
+
+        cell = headerRow.createCell(0);
+        cell.setCellValue("Nama Barang");
+        cell.setCellStyle(headerCellStyle);
+
+        cell = headerRow.createCell(1);
+        cell.setCellValue("Jumlah Stok");
+        cell.setCellStyle(headerCellStyle);
+
+        cell = headerRow.createCell(2);
+        cell.setCellValue("Type");
+        cell.setCellStyle(headerCellStyle);
+
+        cell = headerRow.createCell(3);
+        cell.setCellValue("Keterangan");
+        cell.setCellStyle(headerCellStyle);
+
+        cell = headerRow.createCell(4);
+        cell.setCellValue("Date");
+        cell.setCellStyle(headerCellStyle);
+
+        cell = headerRow.createCell(5);
+        cell.setCellValue("Stok Akhir");
+        cell.setCellStyle(headerCellStyle);
+    }
+
+    /**
+     * Fills Data into Excel Sheet
+     * <p>
+     * NOTE: Set row index as i+1 since 0th index belongs to header row
+     *
+     * @param dataList - List containing data to be filled into excel
+     */
+    private static void fillDataIntoExcel(List<InventoryData> dataList) {
+        for (int i = 0; i < dataList.size(); i++) {
+            // Create a New Row for every new entry in list
+            Row rowData = sheet.createRow(i + 1);
+
+            // Create Cells for each row
+            cell = rowData.createCell(0);
+            cell.setCellValue(dataList.get(i).getNamaBarang());
+
+            cell = rowData.createCell(1);
+            cell.setCellValue(dataList.get(i).getJmlStok());
+
+            cell = rowData.createCell(2);
+            cell.setCellValue(dataList.get(i).getType());
+
+            cell = rowData.createCell(3);
+            cell.setCellValue(dataList.get(i).getKet());
+
+            cell = rowData.createCell(4);
+            cell.setCellValue(dataList.get(i).getDate());
+
+            cell = rowData.createCell(5);
+            cell.setCellValue(dataList.get(i).getStokAkhir());
+        }
+    }
+
+    /**
+     * Store Excel Workbook in external storage
+     *
+     * @param context  - application context
+     * @param fileName - name of workbook which will be stored in device
+     * @return boolean - returns state whether workbook is written into storage or not
+     */
+    private static boolean storeExcelInStorage(Context context, String fileName) {
+        boolean isSuccess;
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), fileName);
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            workbook.write(fileOutputStream);
+            Log.e("testa", "Writing file" + file);
+            isSuccess = true;
+
+        } catch (IOException e) {
+            Log.e("testa", "Error writing Exception: ", e);
+            isSuccess = false;
+        } catch (Exception e) {
+            Log.e("testa", "Failed to save file due to Exception: ", e);
+            isSuccess = false;
+        } finally {
+            try {
+                if (null != fileOutputStream) {
+                    fileOutputStream.close();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        return isSuccess;
+    }
+
+    //endregion
 }
